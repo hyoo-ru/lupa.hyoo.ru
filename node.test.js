c1b2ccad53dadfde4122fc5ff00659ea14dbe580
nin-jin <nin-jin@users.noreply.github.com>
2025-10-08 07:15:08 +0000

    Deploying to gh-pages from @ hyoo-ru/lupa.hyoo.ru@a383171e74c245a04d84f5b9dde75b904b4d1fd9 🚀

--- node.test.js
+++ node.test.js
@@ -1170,6 +1170,7 @@ var $;
         }
         destructor() {
             super.destructor();
+            $mol_wire_fiber.planning.delete(this);
             if (!$mol_owning_check(this, this.cache))
                 return;
             try {
