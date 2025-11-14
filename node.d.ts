5e679a9692cd7955b2a61f8a90d561336cf0d92b
nin-jin <nin-jin@users.noreply.github.com>
2025-11-14 07:16:55 +0000

    Deploying to gh-pages from @ hyoo-ru/lupa.hyoo.ru@a383171e74c245a04d84f5b9dde75b904b4d1fd9 🚀

--- node.d.ts
+++ node.d.ts
@@ -1238,6 +1238,10 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    function $mol_error_message(this: $, error: unknown): string;
+}
+
+declare namespace $ {
     function $mol_dom_render_styles(el: Element, styles: {
         [key: string]: string | number;
     }): void;
